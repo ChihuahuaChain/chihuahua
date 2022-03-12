9ce618fc178bcc6344b97386d9691516e8b9e98d
faddat <jacobgadikian@gmail.com>
2022-03-12 22:58:19 +0000

    update cw, starport, ibc, tendermint, sdk

--- cmd/chihuahuad/main.go
+++ cmd/chihuahuad/main.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/ChihuahuaChain/chihuahua/app"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
-	"github.com/tendermint/spm/cosmoscmd"
+	"github.com/tendermint/starport/starport/pkg/cosmoscmd"
 )
 
 func main() {

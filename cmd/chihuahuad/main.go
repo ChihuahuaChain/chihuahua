2ab5816aff441e0bebbda06cfe26874db12b3819
Jacob Gadikian <jacobgadikian@gmail.com>
2022-10-22 17:43:27 +0700

    more cmd folder: wasm genesis stuff

--- cmd/chihuahuad/main.go
+++ cmd/chihuahuad/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"os"
 
+	"github.com/ChihuahuaChain/chihuahua/cmd/chihuahuad/cmd"
 	"github.com/cosmos/cosmos-sdk/server"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/cosmos/cosmos-sdk/simapp"
-	"github.com/cosmos/cosmos-sdk/simapp/simd/cmd"
 )
 
 func main() {

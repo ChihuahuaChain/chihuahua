388126d1ca909e07f089f4db7175439aa76e0689
bulldogjack <bulldogjack@pm.me>
2022-10-26 20:34:29 -0400

    Set the node home correctly.

--- cmd/chihuahuad/main.go
+++ cmd/chihuahuad/main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"os"
 
+	"github.com/ChihuahuaChain/chihuahua/app"
 	"github.com/ChihuahuaChain/chihuahua/cmd/chihuahuad/cmd"
 	"github.com/cosmos/cosmos-sdk/server"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
-	"github.com/cosmos/cosmos-sdk/simapp"
 )
 
 func main() {
 	rootCmd, _ := cmd.NewRootCmd()
 
-	if err := svrcmd.Execute(rootCmd, simapp.DefaultNodeHome); err != nil {
+	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
 		switch e := err.(type) {
 		case server.ErrorCode:
 			os.Exit(e.Code)

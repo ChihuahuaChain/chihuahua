00ae3b03c50756654aed65c010005f7ac42f14e0
lydia.pierce@principia.edu <lydia.pierce@principia.edu>
2023-01-27 10:13:44 -0500

    move relayers to other repo

--- tokentransfer/chihuahua/build-images.sh
+++
@@ -1,2 +0,0 @@
-docker build -f Dockerfile . -t chihuahuaa --build-arg configyml=./configa.yml --no-cache
-docker build -f Dockerfile . -t chihuahuab --build-arg configyml=./configb.yml --no-cache
--- tokentransfer/chihuahua/run-chihuahua.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ignite chain serve

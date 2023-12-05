573684ba138d2f9c192e38c8b5f423fe57de79a4
Reece Williams <reecepbcups@gmail.com>
2023-12-05 15:43:55 -0600

    fix so protocgen gens other namespaces

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 echo "Generating gogo proto code"
 cd proto
-proto_dirs=$(find ./chihuahua -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find ./ -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep go_package "$file" &>/dev/null; then
